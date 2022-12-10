1cde8897c7c5c1845a91d24b5e5e772c29988b72
alon651 <52508633+alon651@users.noreply.github.com>
2022-12-10 20:05:14 +0200

    stupid fixes

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::io::{BufReader, Read};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    // println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
     let args: Vec<String> = env::args().collect();
